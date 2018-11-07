fa050f830ba5c3addc34dfe541c6c8fe60e1f8fa
David Krieger <dkrieger@pivotal.io>
2018-11-07 11:33:02 -0800

    actual repeat script run on 20111106 on gcp

--- contrib/pg_upgrade/repeat_perftest.sh
+++ contrib/pg_upgrade/repeat_perftest.sh
@@ -3,22 +3,23 @@
 
 double_size()
 {
-    echo "doubling size..."
-    echo `date`
-    gpstart -a
-    psql -d postgres -c "insert into erase select * from erase;"
-    psql -d postgres -c "checkpoint;"
-    psql -d postgres -c "vacuum;"
-    psql -d postgres -c "select gp_segment_id, count(gp_segment_id) from erase group by gp_segment_id;"
-    gpstop -a
+	echo "doubling size..."
+	echo `date`
+	gpstart -a
+	psql -d postgres -c "insert into foo select * from foo;"
+	psql -d postgres -c "checkpoint;"
+	psql -d postgres -c "vacuum;"
+	psql -d postgres -c "select gp_segment_id, count(gp_segment_id) from foo group by gp_segment_id;"
+	gpstop -a
 }
 
-for i in 1 2
+for i in 2 4 8 16
 do
-    for j in 1 2
-    do
-	echo "running perfcheck..."
-	make perfcheck  > "20181106_$i_$j.log" 2>&1
-    done
-    double_size
+	for j in 1 2 3 
+	do
+		echo "running perfcheck..."
+		make perfcheck  > "20181106_$i$j.log" 2>&1
+	done
+	double_size
 done
+
